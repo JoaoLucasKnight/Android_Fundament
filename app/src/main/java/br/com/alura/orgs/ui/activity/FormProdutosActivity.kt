45e86807fbde121a9952770a7eaec3221dc03fc3
JoaoLucasKnight <kirajota1@gmail.com>
2024-03-14 13:19:37 -0300

    Aula completa

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormProdutosActivity.kt
@@ -0,0 +1,17 @@
+package br.com.alura.orgs.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import br.com.alura.orgs.R
+
+class FormProdutosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_produtos)
+
+    }
+}
+
+// == class FormprodutosActivity :
+// AppCompatActivity(R.layout.activity_form_produtos)
